c17d7c962b2e20adadc980e468b3233046f9c8f6
axolotl-teddy-bear <wonginseong@gmail.com>
2025-10-20 17:04:20 +0100

    edit

---
+++ 03.multiples/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(300);
+  
+  textFont('Times New Roman')
+  textAlign(CENTER);
+  textSize(16)
+  text('Please enter your username', 300, 150)
+  
+}
