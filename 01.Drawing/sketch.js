6957e84f9bf641dffa03fbc007b1d9f879416075
Mikaela Wong <wonginseong@gmail.com>
2025-11-05 11:32:48 +0000

    edit

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -5,7 +5,8 @@ function preload() {
   */
 
 function setup() {
-  createCanvas(550, 400);
+  strokeWeight(0);
+  createCanvas(600, 400);
 }
 
 function draw() {
@@ -16,84 +17,84 @@ function draw() {
   rec_ay=380
   rec_by=550
   
-  rect(195, 20, 170, 370)
+  //rect(195, 20, 300, 370);
   
-  applyMatrix(1, 0, 0, 1, 120, -80) //i added this matrix so that moving the entire image on the background would be easier. this is one of my last moves so it didnt interfere with the coding process much. 
-  scale(0.8)
+  applyMatrix(1, 0, 0, 1, 120, -80); //i added this matrix so that moving the entire image on the background would be easier. this is one of my last moves so it didnt interfere with the coding process much. 
+  scale(0.8);
   
   //body 
   push();
   fill(250);
   strokeWeight(0);
-  rect(150, 190, 100, 230) //body
+  rect(150, 190, 100, 230); //body
   push();
-  fill(0)
-  triangle(150, 190, 147, 385, 128, 188)//black outline
-  triangle(250, 190, 253, 385, 272, 192)
+  fill(0);
+  triangle(150, 190, 147, 385, 128, 188);//black outline
+  triangle(250, 190, 253, 385, 272, 192);
   pop();
   
   //water
   push();
-  fill('lightblue')
-  rect(152.5, 190, 95, 170, 15, 15, 15, 15)
+  fill('lightblue');
+  rect(152.5, 190, 95, 170, 15, 15, 15, 15);
   pop();
   
   //these three are white circle that overlay the light blue rectangle i've made. this makes a wave pattern that Im quite proud of
-  circle(200, 210, 40)
-  circle(165, 210, 40)
-  circle(235, 210, 40)
+  circle(200, 210, 40);
+  circle(165, 210, 40);
+  circle(235, 210, 40);
 
-  triangle(150, 190, 150, 400, 130, 190)
-  triangle(250, 190, 250, 400, 270, 190)
+  triangle(150, 190, 150, 400, 130, 190);
+  triangle(250, 190, 250, 400, 270, 190);
   
   push();
   fill(0);
-  triangle(128, 178, 107, 178, 128, 202)
+  triangle(128, 178, 107, 178, 128, 202);
   pop();
-  triangle(128, 180, 110, 180, 128, 200)
+  triangle(128, 180, 110, 180, 128, 200);
   
   //cover
   push(); //black outline
-  fill(0)
-  rect(163, 148, 71, 31)
-  triangle(163, 148, 158, 148, 163, 165)
-  triangle(233, 148, 242, 148, 237, 165)
+  fill(0);
+  rect(163, 148, 71, 31);
+  triangle(163, 148, 158, 148, 163, 165);
+  triangle(233, 148, 242, 148, 237, 165);
   pop();
   
-  rect(165, 150, 70, 30)
-  triangle(165, 150, 160, 150, 165, 165)
-  triangle(235, 150, 240, 150, 235, 165)
+  rect(165, 150, 70, 30);
+  triangle(165, 150, 160, 150, 165, 165);
+  triangle(235, 150, 240, 150, 235, 165);
   
-  fill(250)
-  rect(167, 152, 65, 25)
-  triangle(167, 152, 164, 152, 167, 165)
-  triangle(232, 152, 236, 152, 232, 165)
+  fill(250);
+  rect(167, 152, 65, 25);
+  triangle(167, 152, 164, 152, 167, 165);
+  triangle(232, 152, 236, 152, 232, 165);
   
-  fill(0)
-  rect(128, 180, 144, 25)
-  ellipse(200, 180, 144, 40)
-  fill(250)
+  fill(0);
+  rect(128, 180, 144, 25);
+  ellipse(200, 180, 144, 40);
+  fill(250);
   
   //reflections
   fill(150);
-  rect(150, 180, 25, 24)
-  rect(225, 180, 25, 24)
-  fill(200)
-  rect(160, 180, 10, 24)
-  rect(230, 180, 10, 24)
+  rect(150, 180, 25, 24);
+  rect(225, 180, 25, 24);
+  fill(200);
+  rect(160, 180, 10, 24);
+  rect(230, 180, 10, 24);
   pop();
 
   
   
   //the base of the blender
   fill(100);
-  ellipse(200, rec_by, 145, 35)
+  ellipse(200, rec_by, 145, 35);
   ellipse(200, rec_ay, 105, 30);
   fill(0);
-  rect(150, 380, 100, 170)
-  triangle(rec_ax, rec_ay, rec_ax, rec_by, rec_ax-20, rec_by)
-  triangle(rec_bx, rec_ay, rec_bx, rec_by, rec_bx+20, rec_by)
-  ellipse(200, rec_by, 140, 20)
+  rect(150, 380, 100, 170);
+  triangle(rec_ax, rec_ay, rec_ax, rec_by, rec_ax-20, rec_by);
+  triangle(rec_bx, rec_ay, rec_bx, rec_by, rec_bx+20, rec_by);
+  ellipse(200, rec_by, 140, 20);
   ellipse(200, rec_ay, 100, 20);
   
   //reflections
@@ -110,16 +111,16 @@ function draw() {
   
   push();
   //circle(200, 466, 5) //center of base
-  translate(rec_ax+85, rec_ay+86)
-  rotate(-0.05)
+  translate(rec_ax+85, rec_ay+86);
+  rotate(-0.05);
   rect(-13, -93, 15, 184);
   //circle(0, 0, 5) //origin point 
   pop();
   
   fill(200);
   push();
-  translate(rec_ax+85, rec_ay+86)
-  rotate(-0.05)
+  translate(rec_ax+85, rec_ay+86);
+  rotate(-0.05);
   rect(-10, -93, 8, 184);
   pop();
   
@@ -132,39 +133,39 @@ function draw() {
   
   //button
   push();
-  fill(100)
-  strokeWeight(5)
-  circle(200, 480, 50)
+  fill(100);
+  strokeWeight(5);
+  circle(200, 480, 50);
   pop();
   
   push();
-  fill(0)
-  rect(195, 455, 10, 20)
+  fill(0);
+  rect(195, 455, 10, 20);
   
-  fill(250)
-  translate(200, 480)
-  circle(0, 0, 5)
+  fill(250);
+  translate(200, 480);
+  circle(0, 0, 5);
   pop();
   
   
   //fish 
   push();
-  strokeWeight(0)
-  fill(184, 124, 22)
-  ellipse(205, 270, 53, 30)
-  triangle(179, 264, 210, 255, 205, 240)
-  triangle(190, 270, 170, 300, 160, 240)
+  strokeWeight(0);
+  fill(184, 124, 22);
+  ellipse(205, 270, 53, 30);
+  triangle(179, 264, 210, 255, 205, 240);
+  triangle(190, 270, 170, 300, 160, 240);
   pop();
   
   fill('lightblue');
-  strokeWeight(0)
-  triangle(175, 270, 170, 300, 160, 240)
+  strokeWeight(0);
+  triangle(175, 270, 170, 300, 160, 240);
   
-  fill(255, 200)
-  circle(220, 269, 10)
-  fill(0)
-  circle(220, 269, 6)
+  fill(255, 200);
+  circle(220, 269, 10);
+  fill(0);
+  circle(220, 269, 6);
   
-  fill(255)
+  fill(255);
 }
 
--- 02.movement/sketch.js
+++ 02.movement/sketch.js
@@ -1,26 +1,20 @@
 function setup() {
-  createCanvas(500, 500);
-  noStroke();
-  background(0,0,0);
-  fill(255,200,255);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(0,0,0,10);
-  x1 = mouseX + random(-50, 50);
-  x2 = mouseX + random(-50, 50);
-  y1 = mouseY + random(-50, 50);
-  y2 = mouseY + random(-50, 50);
-
-  cl1 = random(0, 255);
-  cl2 = random(0, 255);
-  cl3 = random(0, 255);
-
-  let value = color(cl1, cl2, cl3);
+  background(220);
   
-  //trail.push(createVector (mouseX, mouseY));
-  
-  fill(value);
-  triangle(x1, y1, x2, y2, mouseX, mouseY); 
+  stroke(180);
+  line(300, 0, 300, 600);
+
+  circle(300, 150, 50)
+
+  push();
+    translate(600, 0); //move the horizontal origin to the far right side of the canvas
+    scale(-1, 1); //flip in the horizontal direction
+
+
+  pop();
 
 }
